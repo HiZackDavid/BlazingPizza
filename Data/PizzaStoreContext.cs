bac2bd3757f0006da901e5dc8cae5dd607ea66f0
Isaac David Zolana <HiZackDavid@hotmail.com>
2023-08-28 00:19:12 -0400

    Add Blazor app files

---
+++ Data/PizzaStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data;
+
+/// <summary>
+/// This class creates a database context we can use to register a database service. The context also allows us to have a controller that accesses the database.
+/// </summary>
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions options) : base(options) 
+    {
+    }
+
+    public DbSet<PizzaSpecial> Specials { get; set; }
+}
